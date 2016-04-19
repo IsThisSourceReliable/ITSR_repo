8b5abc5669f0e0add98b43bf98436c5d5ee9fa16
Martin <myrtass@gmail.com>
2016-04-19 18:22:58 +0200

    Classes

---
+++ ITSR/CLASSES/USER/Password.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITSR.CLASSES.USER
+{
+    public class Password
+    {
+        public int user_id { get; set; }
+        public string passwordInput { get; set; }
+        public string passwordOrg { get; set; }
+        
+
+        //Methods
+        public void CreateSecurePassword()
+        {
+            
+        }
+        public void TryPassword()
+        {
+
+        }
+        public void CreateSalt()
+        {
+
+        }
+        public void SaltAndHashPassword()
+        {
+
+        }
+        public void SplitSaltFromHash()
+        {
+
+        }
+        public void CompareSaltHash()
+        {
+
+        }
+        public void GetPasswordFromDb()
+        {
+
+        }
+    }
+}
