a2fc6c078e2d8896e7883265bb4b1105eb7774c7
Martin <myrtass@gmail.com>
2016-04-24 00:28:54 +0200

    moooore methods blocking, banning, making moderators and certfieing members

--- ITSR/CLASSES/USER/Administrator.cs
+++ ITSR/CLASSES/USER/Administrator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,97 @@ namespace ITSR.CLASSES.USER
 {
     public class Administrator:Moderator
     {
-        
+
+        MySqlConnection conn = new MySqlConnection("Database=itsrdb; Data Source=eu-cdbr-azure-north-e.cloudapp.net; User Id=b268b5fbbce560; Password=d722d6d4");
+
+        //Methods
+        public void UpdateMemberToModerator(Member m)
+        {
+            string sql = "Update user SET role_id = @RID WHERE iduser = @ID";
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@RID", 2);
+                cmd.Parameters.AddWithValue("@ID", m.ID);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void UpdateModeratorToAdministrator(Moderator m)
+        {
+            string sql = "Update user SET role_id = @RID WHERE iduser = @ID";
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@RID", 3);
+                cmd.Parameters.AddWithValue("@ID", m.ID);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void MakeMemberCertified(Member m)
+        {
+            string sql = "Update user SET certfied_user = @CEU WHERE iduser = @ID";
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@CEU", true);
+                cmd.Parameters.AddWithValue("@ID", m.ID);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void MakeCertifiedMemberNotCertified(Member m)
+        {
+            string sql = "Update user SET certfied_user = @CEU WHERE iduser = @ID";
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@CEU", false);
+                cmd.Parameters.AddWithValue("@ID", m.ID);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
