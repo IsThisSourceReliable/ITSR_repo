2b11b1ed718568a80d692520c0cf35006b2a1c4a
Martin <myrtass@gmail.com>
2016-05-10 13:54:46 +0200

    profilepage

--- ITSR/About.aspx
+++ ITSR/About.aspx
@@ -1,9 +1,13 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMaster.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ITSR.About" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="CSS/AboutCSS.css" rel="stylesheet" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentHolder" runat="server">
     <div class="fullBox white-box">
-        <div class="fullBox">Titel</div>
-        <div class="fullBox "></div>
+        <div class="fullBox fullbox-about">
+            <h1>Titel</h1>
+            <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
+        </div>
+        
     </div>
 </asp:Content>
---
+++ ITSR/Profile.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ITSR.CLASSES.USER;
+using System.Data;
+
+namespace ITSR
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadProfilePage(21);
+        }
+
+        public void LoadProfilePage(int iduser)
+        {
+            Member m = new Member();
+
+            m.ID = iduser;
+            m.LoadUser(m);
+            m.LoadUserProfile(m);
+
+            lbluserName.Text = m.userName;
+            lblFullNAme.Text = m.firstname + " " + m.lastName;
+            lblCountry.Text = m.country;
+            lblLocation.Text = m.location;
+            lblOccupation.Text = m.occupation;
+            lblAboutme.Text = m.aboutme;
+            
+
+            populateGvMyArticles(m.GetArticlesCreatedBy(m));
+            populateGvVotes(m.GetLastVotesBy(m));
+        }
+        public void populateGvMyArticles(DataTable dt)
+        {
+            gvMyArticles.DataSource = dt;
+            gvMyArticles.DataBind();
+        }
+        public void populateGvVotes(DataTable dt)
+        {
+            gvMyVotes.DataSource = dt;
+            gvMyVotes.DataBind();
+        }
+
+        protected void gvMyArticles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["articleID"] = Convert.ToInt32(gvMyArticles.SelectedValue);
+            Response.Redirect("~/Article.aspx");
+        }
+        protected void gvMyArticles_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // Attaching one onclick event for the entire row, so that it will fire SelectedIndexChanged, while we click anywhere on the row.
+                e.Row.Attributes["onclick"] =
+                  ClientScript.GetPostBackClientHyperlink(gvMyArticles, "Select$" + e.Row.RowIndex);
+            }
+        }
+        protected void gvMyVotes_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // Attaching one onclick event for the entire row, so that it will fire SelectedIndexChanged, while we click anywhere on the row.
+                e.Row.Attributes["onclick"] =
+                  ClientScript.GetPostBackClientHyperlink(gvMyVotes, "Select$" + e.Row.RowIndex);
+            }
+        }
+        protected void gvMyVotes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["articleID"] = Convert.ToInt32(gvMyVotes.SelectedValue);
+            Response.Redirect("~/Article.aspx");
+        }
+    }
+}
